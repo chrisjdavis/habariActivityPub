0b1445be62c0fb2fcc887f9d42b5888c6e5fbad8
Chris J. Davis <cjd@chrisjdavis.org>
2018-08-20 00:49:10 -0700

    Some updates for the working times
    We can now accept posts to a users outbox, and posts to a users inbox, and we can have a post to an outbox trigger a post to another users inbox. Spec'ing out following right now.

--- activityPub.plugin.php
+++ activityPub.plugin.php
@@ -6,6 +6,12 @@ class aPub extends Plugin
 	const EXECUTED = 1;
 	const NOT_EXECUTED = 1;
 	
+	const ACTION_FOLLOW = 1;
+	const ACTION_UNFOLLOW = 2;
+	const ACTION_BLOCK = 3;
+	const ACTION_LIKE = 4;
+	const ACTION_NOTE = 5;
+		
 	public function filter_autoload_dirs($dirs) {
 		$dirs[] = __DIR__ . '/classes';
 		
@@ -35,13 +41,15 @@ class aPub extends Plugin
 	# TODO: define what structure this table should have.
 	#
 	private function create_activitypub_inbox() {
-		$sql = "CREATE TABLE " . DB::table('inbox') . " (
-				id int unsigned NOT NULL AUTO_INCREMENT,0',
-				raw_json varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-				executed int(11) unsigned DEFAULT '0',
-				PRIMARY KEY (id),
-				) DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;";
-			
+		$sql = "CREATE TABLE {\$prefix}inbox (
+			  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+			  `action` int(11) unsigned NOT NULL,
+			  `raw_json` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+			  `executed` int(11) unsigned NOT NULL,
+			  `user_id` int(11) unsigned DEFAULT NULL,
+			  PRIMARY KEY (`id`)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
+		
 		DB::dbdelta( $sql );
 	}
 	
@@ -50,12 +58,14 @@ class aPub extends Plugin
 	# TODO: define what structure this table should have.
 	#
 	private function create_activitypub_outbox() {
-		$sql = "CREATE TABLE " . DB::table('outbox') . " (
-				id int unsigned NOT NULL AUTO_INCREMENT,0',
-				raw_json varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-				executed int(11) unsigned DEFAULT '0',				
-				PRIMARY KEY (id),
-				) DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;";
+		$sql = "CREATE TABLE {\$prefix}outbox (
+			  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+			  `action` int(11) unsigned NOT NULL,			  
+			  `raw_json` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+			  `executed` int(11) unsigned NOT NULL,
+			  `user_id` int(11) unsigned DEFAULT NULL,
+			  PRIMARY KEY (`id`)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
 			
 		DB::dbdelta( $sql );
 	}
@@ -65,12 +75,13 @@ class aPub extends Plugin
 	# TODO: define what structure this table should have.
 	#
 	private function create_activitypub_liked() {
-		$sql = "CREATE TABLE " . DB::table('outbox') . " (
-				id int unsigned NOT NULL AUTO_INCREMENT,0',
-				raw_json varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-				executed int(11) unsigned DEFAULT '0',
-				PRIMARY KEY (id),
-				) DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;";
+		$sql = "CREATE TABLE {\$prefix}liked (
+			  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+			  `raw_json` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+			  `executed` int(11) unsigned NOT NULL,
+			  `user_id` int(11) unsigned DEFAULT NULL,
+			  PRIMARY KEY (`id`)
+			) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
 			
 		DB::dbdelta( $sql );
 	}
@@ -91,8 +102,7 @@ class aPub extends Plugin
 	 */
 	private static function insert(array $data, string $table_name) {
 		$insert = DB::insert( DB::table( $table_name ), $data );
-		
-		return $insert;
+		return DB::last_insert_id();
 	}
 	
 	/**
@@ -107,6 +117,10 @@ class aPub extends Plugin
 		// we'll pretend its 1935 in Maybury. We trust everyone!
 		
 		return true;
+	}
+	
+	private function save_to_file($data) {
+		file_put_contents( __DIR__ . '/logs/test.txt', print_r($data, true) );
 	}
 
 	/**
@@ -137,7 +151,38 @@ class aPub extends Plugin
 	 * @example POST /v1/<username>/feed
 	 * @return null
 	 */
-	public function rest_post_v1__username_feed($params) {}
+	public function rest_post_v1__username_feed($params) {
+		$person = User::get_by_name( $params['username'] ); $i = 0;
+		
+		$ins = self::insert( array('raw_json' => $json, 'executed' => 0, 'user_id' => $person->id), DB::table('outbox') );
+		
+		$args = json_decode( $json );
+		
+		switch( $args->{'@type'} ) {
+			case 'Follow' :					
+				$url = $args->{'to'}[0]->{'@id'};
+				
+				$rr = new RemoteRequest( $url );
+				$rr->execute();
+				
+				$response = $rr->get_response_body();
+				$tokenizer = new HTMLTokenizer( $response );
+				
+				$deets = $tokenizer->parse();
+
+				foreach( $deets as $deet ) { $i++;
+					if( isset($deet['attrs']['type']) && $deet['attrs']['type'] == 'application/ld+json' ) {
+						$inbox = json_decode( $deets[$i++]['value'] )->inbox;
+						
+						$req = new RemoteRequest( $inbox, 'GET' );
+						$req->set_params( array('payload' => $json) );
+						$req->execute();
+					}
+				}
+			break;
+		}
+		
+	}
 	
 	/**
 	 * rest_get_v1__username_feed
@@ -157,7 +202,23 @@ class aPub extends Plugin
 	 * @example POST /v1/<username>/inbox
 	 * @return null
 	 */
-	public function rest_post_v1__username_inbox($params) {}
+	public function rest_post_v1__username_inbox($params) {
+		$i = 0; $vars = $_POST; $person = User::get_by_name( $params['username'] );
+		
+		$payload = html_entity_decode( $_GET['payload'] );
+		$decoded = json_decode( $payload );
+		
+		$this->save_to_file( $decoded );
+		
+		$ins = self::insert( array('raw_json' => $payload , 'executed' => 0, 'user_id' => $person->id), DB::table('inbox') );
+		
+		exit();
+		
+		switch( $args->{'@type'} ) {
+			case 'Follow' :
+			break;
+		}
+	}
 	
 	/**
 	 * rest_get_v1__username_inbox
